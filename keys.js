c482960d09f25784d0c4c735157fe75bab2d4552
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-07-23 17:21:54 -0500

    spotify

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '	889145891654946817-IT9YCN76ZaVpFx5NRJo9A0Yztxx553L',
   access_token_secret: '8yreaHrcfWyK2d4ac34lq0b5bHKNCyyJ6odpBniw8Adkx',
 }
+
+exports.spotifyKeys = {
+  id: '0197874c42cb4be3af6199f42fa0eee8',
+  secret: '4e56942c420c4991b6964cb18cbf92ed',
+}
