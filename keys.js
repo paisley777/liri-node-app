e8de8590cbd1ee1411037dd28ce06e8e4fad0824
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-07-24 12:42:40 -0500

    fixed Twitter API call

--- keys.js
+++ keys.js
@@ -3,7 +3,7 @@ console.log('this is loaded');
 exports.twitterKeys = {
   consumer_key: 'yx8O5nWAJgf4ckKJwBZCYUBAE',
   consumer_secret: 'j6JDD3Gwmzz9pQI8lJ8ulKdZKehg28af8G1GQ1DrHSOLBrgBml',
-  access_token_key: '	889145891654946817-IT9YCN76ZaVpFx5NRJo9A0Yztxx553L',
+  access_token_key: '889145891654946817-IT9YCN76ZaVpFx5NRJo9A0Yztxx553L',
   access_token_secret: '8yreaHrcfWyK2d4ac34lq0b5bHKNCyyJ6odpBniw8Adkx',
 }
 
